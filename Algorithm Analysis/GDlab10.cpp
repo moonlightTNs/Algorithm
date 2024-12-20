83328ac9e572af22e199887ed19318e8bba0e73d
moonlightTNs <thanop.view@gmail.com>
2024-12-20 19:41:11 +0700

    20/12/2567

--- Algorithm Analysis/GDlab10.cpp
+++ Algorithm Analysis/GDlab10.cpp
@@ -4,38 +4,34 @@ using namespace std;
 const int MAX_HOUR = 100000; 
 
 int main() {
-    int NumCT;
-    cin >> NumCT;
-
+    int n;
+    cin >> n;
     int timeChanges[MAX_HOUR + 2] = {0}; 
 
-    
-    for (int i = 0; i < NumCT; i++) {
+    for (int i = 0; i < n; i++) {
         int startTime, endTime;
         cin >> startTime >> endTime;
         timeChanges[startTime]++;     
         timeChanges[endTime + 1]--;  
     }
 
-   
     int currentCustomers = 0;  
     int maxCustomers = 0;     
     int peakStartTime = 0;      
     int peakEndTime = 0;      
 
-    for (int hour = 0; hour <= MAX_HOUR; hour++) {
-        currentCustomers += timeChanges[hour]; 
+    for (int i = 0; i <= MAX_HOUR; i++) {
+        currentCustomers += timeChanges[i]; 
 
         if (currentCustomers > maxCustomers) {
             maxCustomers = currentCustomers;
-            peakStartTime = hour;            
-            peakEndTime = hour;              
+            peakStartTime = i;            
+            peakEndTime = i;              
         } else if (currentCustomers == maxCustomers) {
-            peakEndTime = hour; 
+            peakEndTime = i; 
         }
     }
 
-    
     cout << peakStartTime << " " << peakEndTime << " " << maxCustomers << endl;
 
     return 0;
